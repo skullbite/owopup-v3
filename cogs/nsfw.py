329b67444697b457045a4236e1e17b14b57bf286
Chloe Brown <lyric_wolf@outlook.com>
2020-09-19 16:22:41 +0100

    Formatting with black

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord.ext.commands import errors
 from utils.apis import e621, furry
 
+
 class NSFW(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,27 +13,28 @@ class NSFW(commands.Cog):
         if ctx.channel.nsfw:
             return True
         return False
-    
+
     async def cog_command_error(self, ctx, err):
         if isinstance(err, errors.CheckFailure):
-            return await ctx.send("This command can only be ran in either a NSFW channel or DMs.")
-
+            return await ctx.send(
+                "This command can only be ran in either a NSFW channel or DMs."
+            )
 
     @commands.command()
     @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
-    async def yiff(self, ctx, stra: str=None):
+    async def yiff(self, ctx, stra: str = None):
         """Shows yiff (default: gay) type "-s" after the command to view straight yiff"""
         if stra == "-s":
             await furry.bot(ctx, "straight", ctx.author)
         else:
             await furry.bot(ctx, "gay", ctx.author)
-    
+
     @commands.command()
     @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
     async def bulge(self, ctx):
         """What's this?"""
         await furry.bot(ctx, "bulge", ctx.author)
-        
+
     @commands.command(name="e621", aliases=["e6"])
     @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
     async def _e621(self, ctx, *, tags: str):
@@ -41,6 +43,7 @@ class NSFW(commands.Cog):
             await e621(tags=tags, ctx=ctx, to_discord=True)
         except Exception as e:
             await ctx.send(e)
-    
+
+
 def setup(bot):
     bot.add_cog(NSFW(bot))
